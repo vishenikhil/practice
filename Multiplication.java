9196f6f2fd3ef091f699f4b0c82c5703a1f73cb9
Nikhil Vishe <141897230+vishenikhil@users.noreply.github.com>
2023-08-10 14:02:24 +0530

    Add files via upload

---
+++ Multiplication.java
@@ -0,0 +1,22 @@
+package com.addition;
+
+public class Multiplication {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	int a = 10;
+	int b = 20;
+	int mul1 = a*b;
+	System.out.println(mul1);
+	
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
