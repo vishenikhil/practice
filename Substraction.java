874df25a09cd87932ecadf2f00db93d61d90af23
Nikhil Vishe <141897230+vishenikhil@users.noreply.github.com>
2023-08-10 14:18:50 +0530

    Add files via upload

---
+++ Substraction.java
@@ -0,0 +1,20 @@
+package com.addition;
+
+public class Substraction {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	int c = 40;
+	int d = 20;
+	int sub1 = c-d;
+	System.out.println(sub1);
+		
+		
+		
+		
+		
+		
+	}
+
+}
