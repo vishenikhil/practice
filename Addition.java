78ede5dd9fd0f4b9626baabe205e74aa271daed8
Nikhil Vishe <141897230+vishenikhil@users.noreply.github.com>
2023-08-10 13:34:19 +0530

    Add files via upload

---
+++ Addition.java
@@ -0,0 +1,38 @@
+package com.addition;
+
+public class Addition {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int a = 10;
+int b = 20;
+int add = a+b;
+System.out.println("Addition of two numbers = "+ add);
+
+
+int c = 10;
+int d = 20;
+int mul =c*d;
+System.out.println(mul);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	}
+
+}
